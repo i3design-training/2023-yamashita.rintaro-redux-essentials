446583aff00c0cf7d981a0299047a37988e7957a
yamashita.rintaro@R0141git <yamashita.rintaro@i3design.co.jp>
2023-07-21 20:45:10 +0900

    feat: reduxでtodo投稿機能実装

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -9,7 +9,19 @@ const initialState = [
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {},
+  reducers: {
+    // state：現在のReduxストアの状態。この場合、stateは投稿のリストを表す配列
+    // action：ディスパッチされたアクション。このアクションはpayloadプロパティを持つべきで、新しく追加される投稿
+    postAdded(state, action) {
+      // 新しい投稿（action.payload）を投稿のリスト（state）の末尾に追加する
+      // createSlice関数では、Immerというライブラリが内部で使用されている
+      // Immerは、状態を直接変更するようなコードを書くことを許可し、その変更を元に新しい状態を生成する。
+      // そのため、このコードでは状態を直接変更するような操作（state.push）が許可されています。
+      state.push(action.payload)
+    },
+  },
 })
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
