1f13cf5b6b1162fd7cfc83cb180a8a676720cb08
yamashita.rintaro@R0141git <yamashita.rintaro@i3design.co.jp>
2023-07-22 13:38:31 +0900

    feat: Adding a Users Slice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
@@ -12,16 +12,37 @@ const postsSlice = createSlice({
   reducers: {
     // state：現在のReduxストアの状態。この場合、stateは投稿のリストを表す配列
     // action：ディスパッチされたアクション。このアクションはpayloadプロパティを持つべきで、新しく追加される投稿
+    //    type：アクションの種類を示す文字列です。これにより、リデューサーはアクションの種類に基づいて状態の更新方法を決定する
+    //    payload：アクションオブジェクトが持つことができる追加のフィールドで、アクションが運ぶデータを含む。リデューサーが新しい状態を生成するために使用する。
     postAdded(state, action) {
       // 新しい投稿（action.payload）を投稿のリスト（state）の末尾に追加する
       // createSlice関数では、Immerというライブラリが内部で使用されている
       // Immerは、状態を直接変更するようなコードを書くことを許可し、その変更を元に新しい状態を生成する。
-      // そのため、このコードでは状態を直接変更するような操作（state.push）が許可されています。
+      // そのため、このコードでは状態を直接変更するような操作（state.push）が許可されている。
       state.push(action.payload)
+    },
+    prepare(title, content, userId) {
+      return {
+        payload: {
+          id: nanoid(),
+          title,
+          content,
+          user: userId,
+        },
+      }
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      // const existingPost = state.posts.find((post) => post.id === postId)では？
+      const existingPost = state.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
     },
   },
 })
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, postUpdated } = postsSlice.actions
 
 export default postsSlice.reducer
